7b87179c6ab9efd32dcac31eaaf1d3a0c613d47d
stevebetch <stevebetch@1504046f-ec4b-db11-b08a-466b99f6921d>
2015-02-17 01:53:33 +0000

    updated hw1

--- trunk/Homework/hw1/GS.m
+++ trunk/Homework/hw1/GS.m
@@ -1,7 +1,32 @@
-function [ output_args ] = GS( input_args )
-%GS Summary of this function goes here
-%   Detailed explanation goes here
+function  [ x,count ] = GS( A,b,mindiff )
+%GS Custom implementation of the Seidel method
+%   This function takes in the A matrix, the b vector, the maximum number
+%   of iterations, and the minimum error. returns the x values.
+
+if(~any(diag(A)))
+    error 'There is a diagonal entry that is 0!'
+end
+
+D=diag(diag(A));
+E=triu(A-D);
+F=tril(A-D);
+deinv=((D-E)^-1)*F;
+dfinv=(D-F)^-1*b;
+count=0;
+x0=zeros(size(b));
+while(1)
+    count=count+1;
+    x=deinv*x0+dfinv;
+    
+    if(abs(x-x0)<mindiff)
+        break;
+    end
+
+    x0=x;
+end
 
 
 end
+
+
 
