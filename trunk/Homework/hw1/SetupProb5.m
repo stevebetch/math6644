7b6b4a792df44043e670239145b036514a9a6790
stevebetch <stevebetch@1504046f-ec4b-db11-b08a-466b99f6921d>
2015-02-20 04:22:01 +0000

    corrected GS

--- trunk/Homework/hw1/SetupProb5.m
+++ trunk/Homework/hw1/SetupProb5.m
@@ -1,7 +1,7 @@
 %Creates the A matrix for problem 5!
 
 n=1000;
-A=full(gallery('tridiag',n,1,-2-4*(1/1000),1));
+A=full(gallery('tridiag',n,1,-2-4*(1/1000)^2,1));
 b=zeros(n,1);
 b(1)=1;
 b(end)=-2;
