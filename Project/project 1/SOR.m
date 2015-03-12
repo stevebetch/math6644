078275909e17b7d6251ba2a993672c70169bd1d7
stevebetch <stevebetch@1504046f-ec4b-db11-b08a-466b99f6921d>
2015-03-12 02:05:33 +0000

    starting project 1

---
+++ Project/project 1/SOR.m
@@ -0,0 +1,36 @@
+function  [ x,count ] = SOR( A,b,mindiff,w )
+%GS Custom implementation of the Seidel method
+%   This function takes in the A matrix, the b vector, the minimum error 
+%   and the relaxation parameter, w. returns the x values.
+
+if(~any(diag(A)))
+    error 'There is a diagonal entry that is 0!'
+end
+
+D=diag(diag(A));
+d=diag(A);
+E=triu(A-D);
+F=tril(A-D);
+deinv=((D-E)^-1)*F;
+dfinv=(D-F)^-1*b;
+
+ count=0;
+x0=zeros(size(b));
+while(1)
+    count=count+1;
+    xgs=(b-A*x0+D*x0).*(1./d);
+    x=w*xgs+(1-w)*x0;
+
+    if((abs(x-x0)<mindiff))
+        break;
+    end
+    
+    if(count>1000000)
+       break;
+    end
+
+    x0=x;
+end
+
+
+end
