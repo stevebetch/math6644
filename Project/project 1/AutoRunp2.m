7fb166f4f392f149c0cc0c7a49383c972abcb302
stevebetch <stevebetch@1504046f-ec4b-db11-b08a-466b99f6921d>
2015-03-15 05:13:12 +0000

    updating file names

--- Project/project 1/AutoRunp2.m
+++ Project/project 1/AutoRunp2.m
@@ -2,36 +2,45 @@
 %THIS SCRIPT TO SEE THE NUMERICAL METHODS IN ACTION.
 
 clear;
-ex=8:14;
-runs=2.^ex+2;%+2 needed to make n=interior mesh points.
+ex=0:6;
+runs=50*2.^ex; %50,100,200,400,800...
+p=[2,1,1/10,1/100];
+
 h=waitbar(0,'1');
 clear val;
-for a=1:length(runs)
- waitbar(a/length(runs),h,sprintf('%s%d%s%d%s','Running! ',a,' of ', length(runs) ,' running!'));
- 
- n=runs(a);
-[A,b,u]=SetupProb1(n);
+for i=1:length(p)
+    for a=1:length(runs)
+     waitbar(a/length(runs),h,sprintf('%s%d%s%d%s%d','Running! ',a,' of ', length(runs) ,' running! P= ',p(i)));
 
-S=zeros(runs(a));
-Stic(a)=tic;
-k=1:n;
-parfor j=1:n 
-        S(j,k)=sqrt(2/(n+1))*sin((pi*j*k)/(n+1));     
+     n=runs(a);
+    [A,b,u]=SetupProb1(n,p(i));
+
+    
+
+%Begin with Strang's method
+    TsS(a)=toc(Stic(a));
+    PCGticS(a)=tic;
+    [PCGxS,PCGcountS(a) ] = PCGmethod( A,b,C);
+    TpcgS(a)=toc(PCGticS(a));
+
+    CGtic(a)=tic;
+    [CGxS,CGcount(a)]=CGmethod(A,b);
+    TcgS(a)=toc(CGtic(a)); %#ok<*SAGROW>
+    end
+    
+%  Now for Chan's method.
+
+TsC(a)=toc(Stic(a));
+    PCGticC(a)=tic;
+    [PCGxC,PCGcountC(a) ] = PCGmethod( A,b,C);
+    TpcgS(a)=toc(PCGticC(a));
+
+    CGtic(a)=tic;
+    [CGxC,CGcount(a)]=CGmethod(A,b);
+    Tcg(a)=toc(CGtic(a));
+    
+
+
 end
-
-
-Ts(a)=toc(Stic(a));
-% 
-% 
-PCGtic(a)=tic;
-A2=S*A*S';
-[PCGx,PCGcount(a) ] = PCGmethod( A2,b,(A2)^-1);
-Tpcg(a)=toc(PCGtic(a));
-
-CGtic(a)=tic;
-[CGx,CGcount(a)]=CGmethod(A,b);
-Tcg(a)=toc(CGtic(a));
-end
-
 close(h);
 
