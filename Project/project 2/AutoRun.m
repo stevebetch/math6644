38e5d7c2aa5f6307083407790031005c0f4e53e2
stevebetch <stevebetch@1504046f-ec4b-db11-b08a-466b99f6921d>
2015-04-19 17:08:19 +0000

    periodic save. Working on the writeup

--- Project/project 2/AutoRun.m
+++ Project/project 2/AutoRun.m
@@ -3,57 +3,48 @@
 atol=1e-6;
 rtol=1e-6;
 maxIt=10000;
-%% Problem 2, part a
+x0=ones(200,1);
+fhandle=@(x) Eqn(x);
+m=2;
 
-%Setup the problem 
-f1= @(x) 2*x.^2-5; % create a function handle for the problem
-x0=10; %initialize the x val 
-xneg=0.99*x0; %for the secant method, x_(-1) val.
- fprintf('%s\n','Run 1');
-[Nx1,newCount1]=Newton(f1,x0,atol,rtol);
-[Cx1, Ccount1 ] = Chord( f1, x0,atol, rtol,maxIt);
-[ Sx1, Scount1  ] = secant( f1, x0,xneg,atol, rtol,maxIt);
-%% Problem 2, part b
- fprintf('%s\n','Run 2');
+Fstart=tic;
+[ Fx, Fcount, Ferr ] = FixedPoint( fhandle, x0,atol, rtol,maxIt);
+fend=toc(Fstart);
 
-f2= @(x) sin(x)+x;
-x0=0.5;
-xneg=0.99*x0;
+cstart=tic;
+[Cx, Ccount, Cerr ] = Chord( fhandle, x0,atol, rtol,maxIt);
+cend=toc(cstart);
 
-[Nx2,newCount2]=Newton(f2,x0,atol,rtol);
-[Cx2, Ccount2 ] = Chord( f2, x0,atol, rtol,maxIt);
-[ Sx2, Scount2  ] = secant( f2, x0,xneg,atol, rtol,maxIt);
-%% Problem 2, part c
+nstart=tic;
+[Nx,Ncount, Nerr]=Newton( fhandle, x0,atol, rtol,maxIt);
+nend=toc(nstart);
 
- fprintf('%s\n','Run 3');
-f3= @(x) cos(x);
-x0=03;
-xneg=0.99*x0;
+shstart=tic;
+[Shx,Shcount, Sherr]=Shamanskii( fhandle, x0,m,atol, rtol,maxIt);
+shend=toc(shstart);
 
 
-[Nx3,newCount3]=Newton(f3,x0,atol,rtol);
-[Cx3, Ccount3 ] = Chord( f3, x0,atol, rtol,maxIt);
-[ Sx3, Scount3  ] = secant( f3, x0,xneg,atol, rtol,maxIt);
+
+
 
 %% Ploting section figure 1
 figure;
-x=-20:1:20;
-plot(x,f1(x),'DisplayName','Function a')
+
+semilogy(1:Fcount,Ferr,'r--*','DisplayName','Fixed Point Error')
 hold on; 
-plot(10,f1(10),'r*','DisplayName','Starting Point');
-plot(Nx1,f1(Nx1),'c^','DisplayName','Newton''s method End Point');
-plot(Cx1,f1(Nx1),'rd','DisplayName','Chord method End Point');
-plot(Sx1,f1(Nx1),'k*','DisplayName','Secant method End Point');
-plot(x,zeros(length(x),1),'g','DisplayName','Zero-line');
+semilogy(1:Ccount,Cerr,'c--^','DisplayName','Chord Method Error');
+semilogy(1:Ncount,Nerr,'b--d','DisplayName','Newton''s Method Error');
+semilogy(1:Shcount,Sherr,'k--*','DisplayName','Shamanskii''s Method Error');
+
 
 % Create ylabel
-ylabel({'Y-Axis'},'FontSize',20,'Interpreter','latex');
+ylabel({'Relative error of the function'},'FontSize',20,'Interpreter','latex');
 
 % Create xlabel
-xlabel({'X-Axis'},'FontSize',20,'Interpreter','latex');
+xlabel({'Iteration number'},'FontSize',20,'Interpreter','latex');
 
 % Create title
-title({'Function a Start and End Points'},'FontSize',20,...
+title({'Error of each Numerical Method at each iteration'},'FontSize',20,...
     'Interpreter','latex');
 
 % Create legend
@@ -61,74 +52,14 @@ legend1 = legend('show');
 set(legend1,'Interpreter','latex','FontSize',16);
 
 hold off;
-%% Ploting section Figure 2
-figure;
-x=-4:.1:4;
-plot(x,f2(x),'DisplayName','Function b');
 
-hold on;
-
-plot(0.5,f2(0.5),'r*','DisplayName','Starting Point');
-plot(Nx2,f2(Nx2),'c^','DisplayName','Newton''s method End Point');
-plot(Cx2,f2(Cx2),'rd','DisplayName','Chord method End Point');
-plot(Sx2,f2(Sx2),'k*','DisplayName','Secant method End Point');
-
-plot(x,zeros(length(x),1),'g','DisplayName','Zero-line');
-% Create ylabel
-ylabel({'Y-Axis'},'FontSize',20,'Interpreter','latex');
-
-% Create xlabel
-xlabel({'X-Axis'},'FontSize',20,'Interpreter','latex');
-
-% Create title
-title({'Function b Start and End Points'},'FontSize',20,...
-    'Interpreter','latex');
-
-% Create legend
-legend1 = legend('show');
-set(legend1,'Interpreter','latex','FontSize',16);
-
-hold off;
-%% Ploting section Figure 3
-figure;
-x=-8:.1:4;
-plot(x,f3(x),'DisplayName','Function c');
-
-hold on;
-
-plot(03,f3(3),'r*','DisplayName','Starting Point');
-plot(Nx3,f3(Nx3),'r^','DisplayName','Newton''s method End Point');
-% plot(Cx2,f3(Cx2),'rd');
-% plot(Sx3,f3(Sx3),'k*');
-plot(x,zeros(length(x),1),'g','DisplayName','Zero-line');
-% Create ylabel
-ylabel({'Y-Axis'},'FontSize',20,'Interpreter','latex');
-
-% Create xlabel
-xlabel({'X-Axis'},'FontSize',20,'Interpreter','latex');
-
-% Create title
-title({'Function c Start and End Points'},'FontSize',20,...
-    'Interpreter','latex');
-
-% Create legend
-legend1 = legend('show');
-set(legend1,'Interpreter','latex','FontSize',16);
-
-hold off;
 
 
 %% Latex out
-fprintf('function  &Newton''s & Chord Method & Secant Method %s','\\\hline')
-fprintf('a&%.8f& %.8f& %.8f%s \n',Nx1,Cx1,Sx1,'\\\hline')
-fprintf('b&%.8f& %.8f& %.8f%s \n',Nx2,Cx2,Sx2,'\\\hline')
-fprintf('c&%.8f& %s& %s%s \n',Nx3,'Did Not Converge','Did Not Converge','\\\hline')
+fprintf('function  &Iterations Required to converge & Time to Converge & Final Error %s\n','\\\hline')
+fprintf('Fixed Point&%d& %f& %d%s \n',Fcount,fend,Ferr(end),'\\\hline')
+fprintf('Chord&%d& %f& %d%s \n',Ccount,cend,Cerr(end),'\\\hline')
+fprintf('Newton''s Method&%d& %.8f& %d%s\n',Ncount,nend,Nerr(end),'\\\hline')
+fprintf('Shamanskii''s Method&%d& %.8f& %d%s \n',Shcount,shend,Sherr(end),'\\\hline')
 fprintf('\n');
-
-fprintf('function  &Newton''s & Chord Method & Secant Method %s\n','\\\hline')
-fprintf('a&%d& %d& %d%s \n',newCount1,Ccount1,Scount1,'\\\hline')
-fprintf('b&%d& %d& %d%s \n',newCount2,Ccount2,Scount2,'\\\hline')
-fprintf('c&%d& %s& %s%s \n',newCount3,'Did Not Converge','Did Not Converge','\\\hline')
-fprintf('\n');
-
 
--- Project/project 2/Shamanskii.m
+++ Project/project 2/Shamanskii.m
@@ -1,36 +1,35 @@
-function [ x, numIts  ] = Shamanskii( fhandle, x0,atol, rtol)
+function [ x, numIts,stopCheck  ] = Shamanskii( fhandle, x0,m,atol, rtol,maxIt)
 %Shamanskii Shamanskii iteration for non linear systems of equations
 %%This method takes in the function handle to the system that needs to be
 %%solved, the inital x value, and the tolerance.
 %%Since the homework only requires a single nonlinear equation to be
 %%sloved, this method was not extended to cover a matrix. 
-m=2;
+
 r0=norm(fhandle(x0),inf);
 x=x0;
 fx=fhandle(x0);
-numIts=0;
-h=1e-5;
+numIts=1;
 
-while norm(fx,inf)>rtol*r0+atol
-    numIts=numIts+1;
-    df=imag(fhandle(x+h*1i))/h;
+stopVals=rtol*r0+atol;
+stopCheck=r0;
+while stopCheck(numIts)>stopVals && numIts<maxIt
+   
+    Jac=diffjac(x,fhandle,fx);
+    [L,U]=lu(Jac);
     
-    %% Remove this for final runtime calcs!
-%     df2=(fhandle(x+h)-fx)/h;
-%     dabs=abs(df-df2);
-%     if(dabs>.001)
-%         disp('ERROR WITH THE IMAGINARY STEP!!!');
-%     end
-    
-    %% Solve for s. Since this is a 1-d problem, we can just devide by df.
-    %if this was a multi-dimensional matrix, we would need to use a linear
-    %solver to find s. 
-    
-    s=-fx/df;
-    x=x+s;
-    fx=fhandle(x);
-    
-    
+    %% Solve for s. Since this is multi-dimensional matrix,
+    %we need to use a linear solver to find s. 
+    for a=1:m
+        [y,count1]=SOR(L,-fx,10^-6,1.1);
+        [s,count2]=SOR(U,y,10^-6,.95);
+        x=x+s;
+        fx=fhandle(x);
+         numIts=numIts+1;
+        stopCheck(numIts)=norm(fx,inf);
+        if(stopCheck(numIts)<=stopVals)
+            break;
+        end
+    end
 end
 
 
